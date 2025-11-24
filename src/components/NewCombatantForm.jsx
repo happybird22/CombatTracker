67e9d9e7ac1d3686e123b7904b00cd1ee26a4c20
happybird22 <dungeonsnotdating@gmail.com>
2025-11-24 11:21:05 -0700

    Update NewCombatantForm.jsx

--- src/components/NewCombatantForm.jsx
+++ src/components/NewCombatantForm.jsx
@@ -16,6 +16,65 @@ const NewCombatantForm = ({ dispatch }) => {
       initiative: parseInt(initiative),
     };
 
+    <label>
+  Advantage on next turn:
+  <input
+    type="checkbox"
+    checked={newCombatant.advantage}
+    onChange={e =>
+      setNewCombatant({
+        ...newCombatant,
+        advantage: e.target.checked
+      })
+    }
+  />
+</label>
+
+<label>
+  Disadvantage on next turn:
+  <input
+    type="checkbox"
+    checked={newCombatant.disadvantage}
+    onChange={e =>
+      setNewCombatant({
+        ...newCombatant,
+        disadvantage: e.target.checked
+      })
+    }
+  />
+</label>
+
+<label>
+  Condition:
+  <select
+    value={newCombatant.condition}
+    onChange={e =>
+      setNewCombatant({
+        ...newCombatant,
+        condition: e.target.value
+      })
+    }
+  >
+    <option value="">None</option>
+    <option value="blinded">Blinded</option>
+    <option value="charmed">Charmed</option>
+    <option value="deafened">Deafened</option>
+    <option value="frightened">Frightened</option>
+    <option value="grappled">Grappled</option>
+    <option value="incapacitated">Incapacitated</option>
+    <option value="paralyzed">Paralyzed</option>
+    <option value="petrified">Petrified</option>
+    <option value="poisoned">Poisoned</option>
+    <option value="prone">Prone</option>
+    <option value="restrained">Restrained</option>
+    <option value="stunned">Stunned</option>
+    <option value="unconscious">Unconscious</option>
+    <option value="blessed">Blessed</option>
+    <option value="inspiration">Has Inspiration</option>
+    <option value="huntersMark">Hunter’s Mark</option>
+  </select>
+</label>
+
     dispatch({ type: "ADD_COMBATANT", payload: newCombatant });
 
     // Reset form
